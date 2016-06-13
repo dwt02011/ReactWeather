eb716b5e0b02d4e75f93c50673ecde2b8f39e7ec
Daniel Thompson <daniel.thompson@prepfactory.com>
2016-06-13 14:19:51 -0400

    Expanded About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,22 @@ var React = require('react');
 var About = () => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>This is an about page!</p>
+			<h1 className="text-center">About</h1>
+			<p>This is a weather application built on React.
+			I've built this for the Complete React Web App Developer Course.</p>
+			<p>
+				Here are some of the tools I've used:
+			</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a>
+					- This was the JS frameworked used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a>
+					- API used to get weather information.
+				</li>
+			</ul>
 		</div>
 	);
 };
